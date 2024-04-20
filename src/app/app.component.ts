1a12c99c1e70ada8bbbcfbee9f78b7fd617039c1
Murakai <marcelpennt@gmail.com>
2024-04-20 05:18:54 +0200

    added ngx-scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgScrollbarModule } from 'ngx-scrollbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgScrollbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
